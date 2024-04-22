6b23890476c8fb028edb4be65eee7be488930256
Bayrem Gharsellaoui <garssallaoui.bayrem@gmail.com>
2024-04-22 10:20:11 +0000

    Fixed build

--- src/AppEventDrivenFsm.cpp
+++ src/AppEventDrivenFsm.cpp
@@ -13,7 +13,7 @@ typedef enum {
   STATE_MAX,
 } state_t;
 
-/* State machine global context (contains SMF contaxt + user data) */
+/* State machine global context (contains SMF context + user data) */
 typedef struct {
   /* This must be first */
   struct smf_ctx ctx;
@@ -24,10 +24,10 @@ typedef struct {
 } state_machine_t;
 
 static void appThreadHandler();
-static void state0Run(void *machine);
-static void state1Run(void *machine);
+static void state0EventsHandler(void *machine);
+static void state1EventsHandler(void *machine);
 static void setupButton(const struct gpio_dt_spec *buttonGpio, struct gpio_callback *callbackData);
-static void onButtonPress(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
+static void onButtonPressCallback(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
 
 /* Create thread */
 K_THREAD_DEFINE(appThread, 1024, appThreadHandler, NULL, NULL, NULL, 7, 0, 0);
@@ -37,8 +37,8 @@ static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw0), gpi
 
 /* List of states */
 static const struct smf_state states[] = {
-  [STATE_0] = SMF_CREATE_STATE(NULL, state0Run, NULL),
-  [STATE_1] = SMF_CREATE_STATE(NULL, state1Run, NULL),
+  [STATE_0] = SMF_CREATE_STATE(NULL, state0EventsHandler, NULL),
+  [STATE_1] = SMF_CREATE_STATE(NULL, state1EventsHandler, NULL),
 };
 
 /* Global state machine */
@@ -48,6 +48,8 @@ static state_machine_t stateMachine = {0};
 static struct gpio_callback buttonCallbackData = {0};
 
 static void appThreadHandler() {
+  int ret = 0;
+
   /* Configure button GPIO and interrupt */
   setupButton(&button, &buttonCallbackData);
 
@@ -71,16 +73,16 @@ static void appThreadHandler() {
   }
 }
 
-static void state0Run(void *machine) {
-  /* Fiter on events that we need in STATE 0 */
+static void state0EventsHandler(void *machine) {
+  /* Filter on events that we need in STATE 0 */
   if (stateMachine.events & EVENT_BUTTON_PRESS) {
     /* Change state on button press event */
     smf_set_state(SMF_CTX(&stateMachine), &states[STATE_1]);
   }
 }
 
-static void state1Run(void *machine) {
-  /* Fiter on events that we need in STATE 1 */
+static void state1EventsHandler(void *machine) {
+  /* Filter on events that we need in STATE 1 */
   if (stateMachine.events & EVENT_BUTTON_PRESS) {
     /* Change state on button press event */
     smf_set_state(SMF_CTX(&stateMachine), &states[STATE_0]);
@@ -105,11 +107,11 @@ static void setupButton(const struct gpio_dt_spec *buttonGpio, struct gpio_callb
     printk("Error %d: failed to configure interrupt on %s pin %d\r\n", ret, button.port->name, button.pin);
     return;
   }
-  gpio_init_callback(*callbackData, onButtonPress, BIT(button.pin));
-  gpio_add_callback(button.port, *callbackData);
+  gpio_init_callback(callbackData, onButtonPressCallback, BIT(button.pin));
+  gpio_add_callback(button.port, callbackData);
 }
 
-static void onButtonPress(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
+static void onButtonPressCallback(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
   /* Generate button press event */
   k_event_post(&stateMachine.kEvent, EVENT_BUTTON_PRESS);
 }
